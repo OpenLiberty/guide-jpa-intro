e2b87ca9289dff4c38ca2828b8cf9e77f4241fa4
Andrew Deschenes <Andrew.Deschenes@ibm.com>
2018-07-19 08:49:47 -0400

    Added more content to README

--- finish/src/main/java/io/openliberty/guides/jpaguide/dao/EventDao.java
+++ finish/src/main/java/io/openliberty/guides/jpaguide/dao/EventDao.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 import io.openliberty.guides.jpaguide.models.Event;
 
