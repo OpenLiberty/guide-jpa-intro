592f0c9839aa7e49f23870348869724a6e850e22
DannyS03 <danny.salman@ryerson.ca>
2019-11-11 14:19:02 -0500

    indent fix

--- finish/backendServices/src/main/java/io/openliberty/guides/event/models/Event.java
+++ finish/backendServices/src/main/java/io/openliberty/guides/event/models/Event.java
@@ -82,18 +82,18 @@ public class Event implements Serializable {
         return name;
     }
     
-    @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((location == null) ? 0 : location.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result 
-			+ (int) (serialVersionUID ^ (serialVersionUID >>> 32));
-		result = prime * result + ((time == null) ? 0 : time.hashCode());
-		return result;
-    }
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	result = prime * result + ((location == null) ? 0 : location.hashCode());
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	result = prime * result 
+		+ (int) (serialVersionUID ^ (serialVersionUID >>> 32));
+	result = prime * result + ((time == null) ? 0 : time.hashCode());
+	return result;
+}
     
     @Override
     public boolean equals(Object obj) {
