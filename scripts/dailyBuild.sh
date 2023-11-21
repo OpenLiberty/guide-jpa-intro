9a8f0d311e88a94e6ec4936ab623e9c44b946b5e
Gilbert Kwan <gkwan@ca.ibm.com>
2023-11-21 09:12:48 -0500

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-while getopts t:d: flag;
+while getopts t:d:j: flag;
 do
     case "${flag}" in
         t) DATE="${OPTARG}" ;;
         d) DRIVER="${OPTARG}" ;;
+        j) JDK_LEVEL="${OPTARG}" ;;
         *) echo "Invalid option input" ;;
     esac
 done
+
+if [ "$JDK_LEVEL" == "11" ]; then
+    echo "Test skipped because the guide does not support Java 11."
+    exit 0
+fi
 
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" frontendUI/pom.xml
 cat frontendUI/pom.xml
