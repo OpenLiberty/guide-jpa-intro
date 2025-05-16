908d46a139eb8af5c1ea9905579041df8b875e1a
Andrew Sasmito <Andrew.Sasmito@ibm.com>
2025-05-16 11:10:18 -0400

    Update to use maven wrapper
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
 # LMP 3.0+ goals are listed here: https://github.com/OpenLiberty/ci.maven#goals
 
@@ -11,7 +12,7 @@ cd backendServices
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
@@ -24,22 +25,22 @@ mvn -ntp -Dhttp.keepAlive=false \
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-mvn -ntp liberty:start
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp liberty:start
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -ntp failsafe:verify
 
 # Test the frontendUI
 cd ../frontendUI
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn -ntp liberty:start
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp liberty:start
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -ntp failsafe:verify
