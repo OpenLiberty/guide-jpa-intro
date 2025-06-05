85557a58e76fd89cbf7284da1985cb0e3770d97e
Andrew Sasmito <98786007+AndrewSasmito@users.noreply.github.com>
2025-06-05 16:19:32 -0400

    Update to use maven wrapper (#358)
    * Update to use maven wrapper
    
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>
    
    * Updated the test script to work correctly
    
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>
    
    * Updated awkward wording
    
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>
    
    * Update README.adoc
    
    Clarified that the event date should be in the future
    
    * Bump org.junit.jupiter:junit-jupiter-engine in /start
    
    Bumps [org.junit.jupiter:junit-jupiter-engine](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter-engine
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /start
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter-engine in /finish
    
    Bumps [org.junit.jupiter:junit-jupiter-engine](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter-engine
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /finish
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
+./mvnw -version
 
 # LMP 3.0+ goals are listed here: https://github.com/OpenLiberty/ci.maven#goals
 
 # Test the backendServices
-cd backendServices
 
 ## Rebuild the application
 #       package                   - Take the compiled code and package it in its distributable format.
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -pl backendServices \
+    -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
@@ -24,22 +25,28 @@ mvn -ntp -Dhttp.keepAlive=false \
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-mvn -ntp liberty:start
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -pl backendServices \
+    -ntp liberty:start
+./mvnw -pl backendServices \
+    -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -pl backendServices \
+    -ntp failsafe:verify
 
 # Test the frontendUI
-cd ../frontendUI
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -pl frontendUI \
+    -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn -ntp liberty:start
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -pl frontendUI \
+    -ntp liberty:start
+./mvnw -pl frontendUI \
+    -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -pl frontendUI \
+    -ntp failsafe:verify
